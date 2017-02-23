6c736351794e7cbf3c01064d6bdfcf9c22c463bb
Emanuele Canessa <ema0185@gmail.com>
2017-02-23 21:55:48 +0100

    greedy

--- app.py
+++ app.py
@@ -11,17 +11,17 @@ class Cache:
 
     def __init__(self, capacity):
         self.capacity = capacity
-        self.used = 0
         self.endpoints = {}
         self.videos = {}
         self.solution = []
+        self.orded = []
+        self.used = 0
 
     def addEndpoint(self, ide, latency):
         self.endpoints[ide] = latency
 
-    def addVideo(self, idv, size):
-        self.videos[idv] = size
-        self.used += size
+    def addVideo(self, idv, weight):
+        self.videos[idv] = weight
 
     def getEndpoints(self):
         return self.endpoints
@@ -39,12 +39,16 @@ class Endpoint:
         self.latency = latency
         self.videos = {}
         self.caches = {}
+        self.assigned = {}
 
-    def addVideo(self, idv, size):
-        self.videos[idv] = size
+    def addVideo(self, idv, req):
+        self.videos[idv] = req
 
     def addCache(self, idc, latency):
         self.caches[idc] = latency
+
+    def addAssignation(self, idv, idc):
+        self.assigned[idv] = idc
 
     def getVideos(self):
         return self.videos
@@ -55,10 +59,11 @@ class Endpoint:
 def main():
     endpoints = []
     caches = []
-    files = ["me_at_the_zoo.in", "videos_worth_spreading.in", "trending_today.in", "kittens.in"]
+    videos = []
+    files = ["me_at_the_zoo", "videos_worth_spreading", "trending_today", "kittens"]
     print("Start of parsing")
-
-    with open(files[0], 'r') as f:
+    ff = 3
+    with open(files[ff] + ".in", 'r') as f:
         nVideos, nEndpoints, nRequests, nCaches, cacheSize = map(int, f.readline().split(' '))
         #print(nVideos, nEndpoints, nRequests, nCaches, cacheSize)
 
@@ -90,7 +95,42 @@ def main():
 
     print("Start of bestemmie")
 
+    for cache in caches:
+        for endpoint in cache.getEndpoints().items():
+            for video in endpoints[endpoint[0]].getVideos().items():
+                #print(video)
+                if video[0] in cache.getVideos().keys():
+                    cache.getVideos()[video[0]] += video[1]*(endpoints[endpoint[0]].latency-endpoint[1])/videos[video[0]]
+                else:
+                    cache.addVideo(video[0], video[1]*(endpoints[endpoint[0]].latency-endpoint[1])/videos[video[0]])
+
+    for cache in caches:
+        #print(i, sorted(cache.getVideos().items(), key=lambda x:x[1], reverse=True))
+        cache.orded = sorted(cache.getVideos().items(), key=lambda x:x[1], reverse=True)
+        i = 0
+        while cache.used < cacheSize and i < len(cache.orded):
+            if cache.used + videos[cache.orded[i][0]] < cacheSize:
+                cache.solution.append(cache.orded[i][0])
+                cache.used += videos[cache.orded[i][0]]
+            i = i + 1
+
+    invalid = 0
+    for i, cache in enumerate(caches):
+        summa = 0
+        for i in cache.solution:
+            summa += videos[i]
+            #print((videos[i]))
+        assert summa < cacheSize
+        if summa == 0:
+            invalid = invalid + 1
+        #print(summa)
+
+    #print(nCaches-invalid)
+    with open(files[ff] + ".out", "w") as out:
+        out.write(str(nCaches-invalid) + '\n')
+        for i, cache in enumerate(caches):
+            out.write(str(i) + " " + " ".join([str(i) for i in cache.solution]) + '\n')
+
 
 if __name__ == "__main__":
     main()
-
